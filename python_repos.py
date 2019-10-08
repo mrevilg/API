eb50451733efc174f07f34cd1a1aa7623abca4b3
Stephen Clements <mrevilg@gmail.com>
2019-10-08 06:18:19 -0500

    API Response

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,12 @@
 import requests
 
 # Make an API sand store the response.
-url = 
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print("Status code:", r.status_code)
+
+#Store API in a variable.
+response_dict = r.json()
+
+# Process results.
+print(response_dict.keys())
