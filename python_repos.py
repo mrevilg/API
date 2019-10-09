94ec8726b2aab09a57d851a0668547a0d44a0fe3
Stephen Clements <mrevilg@gmail.com>
2019-10-09 06:23:23 -0500

    Multi Repo List

--- python_repos.py
+++ python_repos.py
@@ -5,8 +5,16 @@ url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
 print("Status code:", r.status_code)
 
-#Store API in a variable.
+# Store API in a variable.
 response_dict = r.json()
+print("Total Repositories:", response_dict['total_count'])
 
-# Process results.
-print(response_dict.keys())
+# Explore information about repositories.
+repo_dicts = response_dict['items']
+print("Repositories returned:", len(repo_dicts))
+
+# Examin first repo
+repo_dict = repo_dicts[0]
+print("\nKeys:", len(repo_dict))
+for key in sorted(repo_dict.keys()):
+    print(key)
