f171f3bcf520699f3aa05b13266e49094be21755
Stephen Clements <mrevilg@gmail.com>
2019-10-17 06:25:16 -0500

    Multi-API call

--- hn_submissions.py
+++ hn_submissions.py
@@ -6,3 +6,14 @@ from operator import itemgetter
 url = 'https://hacker-news.firebaseio.com/vO/topstories.json'
 r = requests.get(url)
 print("Status code:", r.status_code)
+
+# Process information about each submission.
+submissions_ids = r.json()
+submissions_dicts = []
+for submissions_id in submissions_ids[:30]:
+    # Make a seperate API call for each submission.
+    url = ('https://hacker-news.firebaseio.com/vO/item/' + 
+            str(submissions_id) + '.json')
+    submission_r = requests.get(url)
+    print(submission_r.status_code)
+    response_dict = submission_r.json()
